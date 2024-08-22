366ab6db70b14d89047a91358536f49c507450b4
Mohsen <somboles@usc.edu>
2024-08-21 19:08:56 -0700

    add publishers

---
+++ multi_legged_controllers/src/command_publishers/goal_publisher.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import PoseStamped
+import sys
+
+def publish_once(goal):
+    # Initialize the ROS node
+    rospy.init_node('goal_publisher_node', anonymous=True)
+
+    # Create a publisher for the /alien2/gait topic with std_msgs/String type
+    pub1 = rospy.Publisher('/alien1/goal', PoseStamped, queue_size=10)
+    pub2 = rospy.Publisher('/alien2/goal', PoseStamped, queue_size=10)
+
+    # Wait for a short time to ensure the publisher is connected
+    rospy.sleep(1)
+
+    # Create the message
+    msg = PoseStamped()
+    msg.pose.position.x = float(goal)
+    msg.pose.position.y = 0
+    msg.pose.position.z = 0
+
+    # Publish the message once
+    pub1.publish(msg)
+    pub2.publish(msg)
+    rospy.loginfo("Published message: %s", msg.pose.position.x)
+
+    # Optionally shut down the node after publishing
+    rospy.signal_shutdown('Published message once')
+
+if __name__ == '__main__':
+    # Get the argument from the command line
+    if len(sys.argv) < 2:
+        print("Please provide the pose goal.")
+        sys.exit(1)
+    goal = sys.argv[1]
+
+    # Call the publish_once function with the argument
+    try:
+        publish_once(goal)
+    except rospy.ROSInterruptException:
+        pass
