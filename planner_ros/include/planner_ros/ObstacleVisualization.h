4b8bce0f4a9714ff7aae2a85311a2a44ead4594d
Mohsen <somboles@usc.edu>
2024-09-06 17:39:27 -0700

    add payload

--- planner_ros/include/planner_ros/ObstacleVisualization.h
+++ planner_ros/include/planner_ros/ObstacleVisualization.h
@@ -65,7 +65,7 @@ public:
 
         marker.pose.position.x = 6.05;
         marker.pose.position.y = 0.0;
-        marker.pose.position.z = 1.45;
+        marker.pose.position.z = 1.5;
 
         markerArray.markers.push_back(marker);
 
@@ -73,11 +73,11 @@ public:
         marker.id = 2;
         marker.scale.x = 0.35;
         marker.scale.y = 0.4;
-        marker.scale.z = 0.55;
+        marker.scale.z = 0.45;
 
         marker.pose.position.x = 8.6;
         marker.pose.position.y = 0.0;
-        marker.pose.position.z = 0.275;
+        marker.pose.position.z = 0.2;
 
         markerArray.markers.push_back(marker);
 
